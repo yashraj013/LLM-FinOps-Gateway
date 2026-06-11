ba84e2085e3183fd5505d5995446b18b1710b0e9
Yashraj <palyash9520@gmail.com>
2026-06-11 14:02:26 +0530

    database connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/database.js';
 
 dotenv.config();  // Load .env file
+
+await connectDB();  // Connect to MongoDB
 
 const app = express();
 const PORT = process.env.PORT || 3000;  // Default to 3000 if not set
